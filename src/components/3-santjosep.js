44d40c754eb7ba6927ba5d1c47705eeb97931d57
adelpozoman <adelpozoman@gmail.com>
2024-05-13 18:18:24 +0200

    sant josep backend

---
+++ src/components/3-santjosep.js
@@ -0,0 +1,27 @@
+const crawler = require('crawler-request');
+
+const dateRegex = /\d{2}\/\d{2}\/\d{4}/;
+
+
+async function do_request(argument){
+    crawler(argument).then(function(response){
+        // handle response
+        //console.log(response);
+        const lines = response.text.split('\n');
+
+        const name = lines[6].trimStart();
+        const id = lines[34];
+        const date = lines[37].match(dateRegex);
+
+        //console.log(name);
+        //console.log(id)
+        //console.log(date[0]);
+
+        return {name, id, date};
+    });
+}
+
+
+//do_request("http://www.santjosepdesatalaia.es/validarcsv.aspx?csv=176522553375331524954755935810");
+
+module.exports = do_request;
