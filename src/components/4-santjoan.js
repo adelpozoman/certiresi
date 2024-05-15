39fa8fe742ad3ec423cd3c4f61d24bcc577e87b9
adelpozoman <adelpozoman@gmail.com>
2024-05-15 13:58:54 +0200

    santjoan major fix and improve. delete certificate and image

--- src/components/4-santjoan.js
+++ src/components/4-santjoan.js
@@ -4,8 +4,6 @@ const fs = require("fs");
 
 
 
-
-
 const { fromPath } = require('pdf2pic');
 const { createWorker } = require('tesseract.js');
 
@@ -32,11 +30,12 @@ function convertTextDateToDDMMYYYY(textDate) {
 }
 
 
-async function main2(){
+async function parse_pdf(destinationPath){
+    const filename = "cer_santjoan_png";
     // Configure pdf2pic
     const options = {
         density: 700,           // output pixels per inch
-        saveFilename: "output", // output file name without extension
+        saveFilename: filename, // output file name without extension
         savePath: "./",         // output path
         format: "png",          // output format
         width: 2480,            // output width
@@ -44,13 +43,13 @@ async function main2(){
         page: 1                 // page number
     };
 
-    // Convert PDF to PNG
-    const converter = fromPath('src/components/downloaded_file2.pdf', options);
+    const converter = fromPath(destinationPath, options);
 
 
     //converter(1).then(async (resolve) => {
         const resolve = await converter(1);
-        console.log("PDF conversion successful");
+        fs.unlink(destinationPath, err => { if (err) console.error(err); });
+        //console.log("PDF conversion successful");
 
         // OCR setup
         const worker = await createWorker("eng")
@@ -61,8 +60,9 @@ async function main2(){
         const { data: { text } } = await worker.recognize(resolve.path);
         //log text extracted from pdf
         //console.log("Extracted Text:", text);
-        // Terminate the worker
-        await worker.terminate();
+        await worker.terminate();  // Terminate the worker
+        //unsync delete png file
+        fs.unlink(resolve.path, err => { if (err) console.error(err); });
 
         const lines = text.split('\n');
         //console.log(lines[7]);
@@ -78,183 +78,95 @@ async function main2(){
         const name = matchName ? matchName[1] : null;
         const id = matchId ? matchId[1] : null;
         const date = convertTextDateToDDMMYYYY(lines[18])
-
-        //console.log(name);
-        //console.log(id);
-        //console.log(date);
-
         return { name, id, date };
-
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-async function do_request(userCode, destinationPath) {
-    let respuesta;
+async function request_pdf(webUrl, destinationPath) {
     try {
-        respuesta = await axios.get(userCode, {
+        const respuesta = await axios.get(webUrl, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/124.0',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/123.0',
                 'Host': 'santjoandelabritja.sedelectronica.es',
             },
             maxRedirects: 0,
-            validateStatus: function (status) {
-                return true; // Always resolve promise for any status code
-            }
-            //validateStatus: (status) => { return true }
+            validateStatus: () => { return true }
         });
-        // process response here
-        await console.log(respuesta.data);
+        //console.log(respuesta.data);
         const cookieHeader = respuesta.headers['set-cookie']
-        //console.log("AAAAAAAAA");
-        //console.log(cookieHeader);
+        //console.log("Gotten cookie is: " + cookieHeader);
 
-        const secondResponse = await axios.get(userCode, {
+        const secondResponse = await axios.get(webUrl, {
             headers: {
                 'Cookie': cookieHeader,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/123.0',
                 'Host': 'santjoandelabritja.sedelectronica.es',
             },
             maxRedirects: 0,
-            validateStatus: function (status) {
-                return true; // Always resolve promise for any status code
-            }
+            validateStatus: () => { return true }
         });
-
         //console.log(secondResponse.data);
 
         const $ = cheerio.load(secondResponse.data);
-
         const link = $('a').filter((index, element) => {
-            // Use a regular expression to match the text with flexible whitespace
-            const regex = /^\s*Descargar una copia\s*$/;
+            const regex = /^\s*Descargar una copia\s*$/;  // Use a regular expression to match the text with flexible whitespace
             return regex.test($(element).text().trim());   //the <a> has trailing and leading whitespace
         }).first();
 
-        // Extract the href attribute from the selected link
-        const href = link.attr('href');
-
-        console.log(href);
-        const finalUrl = "https://santjoandelabritja.sedelectronica.es"+href.substring(2);
-        console.log(finalUrl);
+        const href = link.attr('href');  // Extract the href attribute from the selected link
+        const pdfUrl = "https://santjoandelabritja.sedelectronica.es"+href.substring(2);
+        //console.log(href);
+        //console.log(pdfUrl);
 
 
-
-
-        axios.get(finalUrl, {
+        const pdfResponse = await axios.get(pdfUrl, {
             headers: {
                 'Cookie': cookieHeader,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/123.0',
                 'Host': 'santjoandelabritja.sedelectronica.es',
             },
             responseType: 'stream',
-            }
-            )
-            .then(response => {
-                // Pipe the response stream to a file
-                const writer = fs.createWriteStream(destinationPath);
-                response.data.pipe(writer);
-
-                writer.on('finish', () => {
-                    //console.log('PDF download request success.');
-                });
-
-                writer.on('error', err => {
-                    console.error('Error downloading PDF:', err);
-                });
-            })
-            .catch(error => {
-                console.error('Error fetching PDF:', error);
             });
 
+        const writer = fs.createWriteStream(destinationPath);
+        pdfResponse.data.pipe(writer);
 
+        await new Promise((resolve, reject) => {
+            writer.on('finish', () => {
+                //console.log('PDF download request success.');
+                resolve();
+            });
 
+            writer.on('error', err => {
+                console.error('Error downloading PDF:', err);
+                reject(err);
+            });
+        });
     } catch (error) {
-        console.error('Error trying to request:', error);
+        console.error('Error trying to request data:', error);
     }
-    console.log("AAAAA");
-    //console.log(respuesta);
 }
 
 
 
-async function main(pdfUrl){
-    console.log("pdfUrl: "+pdfUrl);
-    //add .0 at the end of the url
-    //pdfUrl = pdfUrl + ".0";
-    const destinationPath = 'downloaded_file2.pdf'; // Path to save the downloaded PDF
+async function main(webUrl){
+    //console.log("webUrl: "+webUrl);
+    webUrl = webUrl + ".0";  //add .0 at the end of the url
+    const destinationPath = 'cer_santjoan.pdf'; // Path to save the downloaded PDF
 
-    // Download the PDF
     try {
-        await do_request(pdfUrl, destinationPath);
+        await request_pdf(webUrl, destinationPath);
         //console.log('PDF downloaded successfully.');
-
-
-        // Parse the downloaded PDF
-        //save the 3 values given back by PDFI()
-        const { name, id, date } = await main2();
+        const { name, id, date } = await parse_pdf(destinationPath);  // Parse the downloaded PDF
         //console.log("Name: ", name, "Date: ", date, "Id: ", id);
         return {name, date, id};
-
-
-
-        //await parsePDF(destinationPath);
     } catch (error) {
         console.error('Error downloading or parsing PDF:', error);
     }
-
 }
 
-//main('https://santjoandelabritja.sedelectronica.es/doc/66MYS4MRRPHZFPQ3EL2PL54WD.0');
+//main('https://santjoandelabritja.sedelectronica.es/doc/66MYS4MRRPHZFPQ3EL2PL54WD');
 
 module.exports = main;
